09615e314104f75b53f8b1deb030d1201f4cb389
Omar <omar.abdelalim@stryker.com>
2025-07-31 01:15:05 -0400

    log to the console

--- server/generalLogger.js
+++ server/generalLogger.js
@@ -1,15 +1,15 @@
 const { createLogger, transports, format } = require("winston")
 
 const customFormat = format.combine(
-    format.timestamp({format: "YYYY-MM-DD HH:mm:ss"}),
-    format.printf((info) => {
-    return `${info.timestamp} = [${info.level.toUpperCase().padEnd(6)}] - ${info.message}`
-}))
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf(info => `${info.timestamp} = [${info.level.toUpperCase().padEnd(6)}] - ${info.message}`)
+)
 
 const generalLogger = createLogger({
     format: customFormat,
     transports: [
-        new transports.File({filename: "./logs/general.log", level: "silly"})
+        new transports.File({ filename: "./logs/general.log", level: "silly" }),
+        new transports.Console({ level: "silly" })
     ]
 })
 
